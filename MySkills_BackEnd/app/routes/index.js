2952063aceb5ce24e601935e8252521f3d9ba519
Claudia Pires <49255824+Minilunna@users.noreply.github.com>
2024-02-14 20:41:42 +0000

    Delete MySkills_BackEnd directory

--- MySkills_BackEnd/app/routes/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = (app) => {
-    app.use('/api/auth/', require('./auth.routes'));
-    app.use('/api/skills/', require('./skills.routes'));
-    app.use('/api/users/', require('./user.routes'));
-}
